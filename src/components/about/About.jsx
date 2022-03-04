bcff00ad80474f527cb46de6629c2c09f35bf06e
tadzyla <tadas.zvinklys@gmail.com>
2022-03-04 23:06:18 +0000

    making About Me section responsive

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './about.css'
 import  ABOUTME  from '../../assets/about_me.png'
 import { FaAward } from 'react-icons/fa'
+import { SiWebpack } from 'react-icons/si'
+import { GiMountains } from 'react-icons/gi'
 
 const About = () => {
   return (
@@ -19,13 +21,23 @@ const About = () => {
             <article className="about_card">
             < FaAward className='about_icon'/>
               <h5>Experience</h5>
-              <small>1+ year experience</small>
+              <small>1+ year</small>
             </article>
+            <article className="about_card">
+            < SiWebpack className='about_icon'/>
+              <h5>Technologies</h5>
+              <small>HTML CSS JAVASCRIPT REACT NODE</small>
+            </article>
+            <article className="about_card">
+            < GiMountains className='about_icon'/>
+              <h5>Hobbies</h5>
+              <small>Hiking Running Travel Photography</small>
+            </article>
+            </div>
             <p>
               Hello my name is Tadas. I am self taught front end developer.
             </p>
             <a href="#contact" className='btn btn-primary'>Let's Talk!</a>
-          </div>
         </div>
       </container>
     </section>
