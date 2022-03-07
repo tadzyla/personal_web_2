77524f02979a9bc89056ccb2506a9788c938a40c
tadzyla <tadas.zvinklys@gmail.com>
2022-03-07 14:07:12 +0000

    create contact section, make it responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import Portfolio from './components/portfolio/Portfolio'
@@ -15,7 +14,6 @@ const App = () => {
       <About />
       <Portfolio />
       <Contact />
-      <Footer />
     </>
       
   )
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import './contact.css'
+import { AiOutlineMail } from "react-icons/ai"
+import { RiMessengerLine } from 'react-icons/ri'
+import { AiOutlineWhatsApp } from 'react-icons/ai'
 
 const Contact = () => {
   return (
     <section id='contact'>
-      Contact
+      <h5>Get in touch</h5>
+      <h2>Contact me</h2>
+        <div className="container contact_container">
+          <div className="contact_options">
+            <artticle className="contact_option">
+              <AiOutlineMail className='contact_option-icon'/>
+              <h4>Email</h4>
+              <h5>tadas.zvinklys@gmail.com</h5>
+              <a href="mailto:tadas.zvinklys@gmail.com" target="_blank" rel='noreferrer'>Send a message</a>
+            </artticle>
+            <artticle className="contact_option">
+              <RiMessengerLine className='contact_option-icon'/>
+              <h4>Messenger</h4>
+              <h5>Tad As</h5>
+              <a href="https://m.me/tadas.zvinklys.9/" target="_blank" rel='noreferrer'>Send a message</a>
+            </artticle>
+            <artticle className="contact_option">
+              <AiOutlineWhatsApp className='contact_option-icon'/>
+              <h4>WhatsApp</h4>
+              <h5>+4475 . . . 27</h5>
+              <a href="http://api.whatsapp.comsend?phone=447543967427" target="_blank" rel='noreferrer'>Send a message</a>
+            </artticle>
+          </div>
+          <form action="">
+            <input type="text" name='name' placeholder='You Name' required />
+            <input type="emait" name='email' placeholder='You Email' required />
+            <textarea name="message" rows="10" placeholder='Your Message' required></textarea>
+            <button type='submit' className='btn btn-primary'>Send a message</button>
+          </form>
+        </div>
     </section>
   )
 }
